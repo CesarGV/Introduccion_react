5dac84ee580b572e61588ca4fa99c1bf53d2d7cc
César Michel Gaytán Vargas <52634486+CesarGV@users.noreply.github.com>
2021-03-23 20:26:36 -0600

    ADD: Courses component

--- src/counter.js
+++ src/counter.js
@@ -1,24 +1,28 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleClick = (aumentar) => {
+    if (aumentar) {
+      setCount(count + 3);
+    } else {
+      setCount(count - 1);
+    }
   };
 
-  const restar = () => {
-    setCount(count - 1);
-  };
+  useEffect(() => {
+    console.log(`Diste click ${count} veces`);
+  });
 
   return (
     <Fragment>
-      <button className="btn" onClick={() => handleClick()}>
-        Sumar
+      <button className="btn" onClick={() => handleClick(true)}>
+        Aumentar
       </button>
       <br />
-      <button className="btn" onClick={() => restar()}>
-        Restar
+      <button className="btn" onClick={() => handleClick(false)}>
+        Disminuir
       </button>
       <p>Nuestro contador es: {count}</p>
     </Fragment>
